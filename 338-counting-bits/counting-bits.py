3bbf06aef5004bd3c52e51f758ed2d3cf089e2a0
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-08-24 10:01:32 +0530

    Time: 129 ms (7.61%) | Memory: 23.2 MB (42.10%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,11 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         out = [0]
+        count_dict = {0:0}
         for i in range(1,n+1):
             count = 0
             while i > 0:
+                if count_dict.get(i,0):
+                    count+=count_dict[i]
+                    break
                 if i & 1:
                     count +=1
                 i = i >> 1
+
+            count_dict[i] = count
             out.append(count)
         return out
