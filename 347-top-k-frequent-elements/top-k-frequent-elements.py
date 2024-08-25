6befe6153f9f01fe4e7a62ddce7350b1ccb39e94
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-08-25 13:39:10 +0530

    Time: 103 ms (8.51%) | Memory: 21.2 MB (44.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_dict = Counter(nums)
-        freq =[[] for i in range(len(nums)+1)]
-        
-        for key, val in count_dict.items():
-            freq[val].append(key)
-        
-        topk = []
-        for i in range(len(freq)-1, 0, -1):
-            if len(freq[i])>0:
-                for j in freq[i]:
-                    topk.append(j)
-                    if len(topk) == k:
-                        return topk
-        return topk
+        counts = Counter(nums)
+        counts = [(counts[key], key) for key in counts.keys()]
+        counts = sorted(counts, key=temp)[::-1]
+        counts = counts[:k]
+        return [i[-1] for i in counts]
+
+def temp(a):
+    return a[0]
