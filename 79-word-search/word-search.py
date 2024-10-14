170e6041a9624af14c32882b56a252c195d78259
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-14 14:28:08 +0530

    Time: 4067 ms (57.83%) | Memory: 16.6 MB (38.87%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,11 +2,11 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS = len(board)
         COLS = len(board[0])
-        visited = set()
+        # visited = set()
         wordIndex = 0
         
         def dfs(R, C, wordIndex):
-            if ((R, C) in visited
+            if (0
                 or R < 0 or C < 0
                 or R == ROWS or C == COLS
                 or board[R][C] != word[wordIndex]):
@@ -14,14 +14,16 @@ class Solution:
             if wordIndex == len(word)-1:
                 return True
             
-            visited.add((R, C))
+            # visited.add((R, C))
+            board[R][C] = "#"
             directions = [[1,0], [0,1], [-1, 0], [0,-1]]
             for dr, dc in directions:
                 if dfs(R+dr, C+dc, wordIndex+1):
-                    visited.remove((R, C))
+                    # visited.remove((R, C))
                     return True
             
-            visited.remove((R, C))
+            board[R][C] = word[wordIndex]
+            # visited.remove((R, C))
             return False
 
         for R in range(ROWS):
