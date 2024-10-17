9cc3a1d91f170b662c2ed3647f944f5796434fe8
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-17 17:32:03 +0530

    Time: 434 ms (87.48%) | Memory: 41.9 MB (57.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,13 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        grid = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        grid = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
 
-        for r in range(len(text1)-1, -1, -1):
-            for c in range(len(text2)-1, -1, -1):
-                if text1[r] == text2[c]:
-                    grid[r][c] = 1 + grid[r+1][c+1]
+        for R in range(len(text1)-1, -1, -1):
+            for C in range(len(text2)-1, -1, -1):
+                if text1[R] == text2[C]:
+                    grid[R][C] = 1 + grid[R+1][C+1]
                 else:
-                    grid[r][c] = max(grid[r+1][c], grid[r][c+1])
+                    grid[R][C] = max(grid[R+1][C], grid[R][C+1])
         return grid[0][0]
 
-#This is 2DP. Always draw a table. Start from bottom right. If the element matches, do 1+diagdown. Else take max(down, right). Return grid[0][0]
+# 2DP. Always draw a table. Start from bottom right. If the element matches, do 1+diagdown. Else take max(down, right). Return grid[0][0]
+# Optimal solution >> Time : O(n^2). Space : O(n^2)
