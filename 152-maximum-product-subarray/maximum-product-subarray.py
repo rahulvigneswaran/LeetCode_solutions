1831cab53b8977beca83f8fb4f55c888a335a9cd
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-18 23:32:44 +0530

    Time: 9 ms (100.00%) | Memory: 17.3 MB (13.52%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -21,6 +21,6 @@ class Solution:
 # Time complexity >> O(n^2)
 # Space complexity >> O(1)
 
-# Optimized solution >> There are three edges cases. 1) All +ve : Just multply everything. 2) Mixed -ve +ve : If the current number is +ve, the max is the max of previous * current. But if current number is -ve, the max is the min (because this would have the largest absolute value) of previous multiplied by the current number. 3) There is a ZERO : If zero exists, just reset the dp values to 1, so it wont affect the next number's decision.
+# Optimized solution >> There are 4 edges cases. 1) All +ve : Just multply everything. 2) Mixed -ve +ve : If the current number is +ve, the max is the max of previous * current. But if current number is -ve, the max is the min (because this would have the largest absolute value) of previous multiplied by the current number. 3) There is a ZERO : If zero exists, just reset the dp values to 1, so it wont affect the next number's decision. 4) If both the max and min from previous is -ve: When checking max and min, compare with current number too. 
 # Time complexity >> O(n)
 # Space complexity >> O(1)
