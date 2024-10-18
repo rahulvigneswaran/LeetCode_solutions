3d618f2380e82d573224e9fccbffc9b3adc37a12
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-18 23:24:52 +0530

    Time: 7 ms (100.00%) | Memory: 16.9 MB (91.09%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = -float("inf")
+        dpMax = 1
+        dpMin = 1
+
+        for n in nums:
+            if n == 0:
+                dpMax, dpMin = 1, 1
+                res = max(res, n)
+            else:
+                productWithMax = dpMax * n
+                productWithMin = dpMin * n
+                dpMax = max(productWithMax, productWithMin, n)
+                dpMin = min(productWithMax, productWithMin, n)
+                res = max(res, dpMax)
+        return res
+
+
+# Brute Force solution >> Just compare everything with everything.
+# Time complexity >> O(n^2)
+# Space complexity >> O(1)
+
+# Optimized solution >> There are three edges cases. 1) All +ve : Just multply everything. 2) Mixed -ve +ve : If the current number is +ve, the max is the max of previous * current. But if current number is -ve, the max is the min (because this would have the largest absolute value) of previous multiplied by the current number. 3) There is a ZERO : If zero exists, just reset the dp values to 1, so it wont affect the next number's decision.
+# Time complexity >> O(n)
+# Space complexity >> O(1)
