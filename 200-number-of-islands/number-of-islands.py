b32a8fa179144ca8a3961ac0b14d0166bd96b269
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-19 23:32:14 +0530

    Time: 342 ms (5.17%) | Memory: 24.4 MB (26.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        res = 0
-        visited = set()
+        if not grid or not grid[0]:
+            return 0
 
-        def dfs(R, C):
-            if (R not in range(ROWS)
-                or C not in range(COLS)
-                or (R, C) in visited 
-                or grid[R][C] == "0"):
+        islands = 0
+        visit = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (
+                r not in range(rows)
+                or c not in range(cols)
+                or grid[r][c] == "0"
+                or (r, c) in visit
+            ):
                 return
-            visited.add((R, C))
-            dirs = [[1,0],[0,1],[-1,0],[0,-1]]
-            for dr, dc in dirs:
-                dfs(R+dr, C+dc)
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] != "0" and (r,c) not in visited:
-                    res+=1
+
+            visit.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    islands += 1
                     dfs(r, c)
-        
-        return res
+        return islands
 
 # Time complexity >> O(mn*4^(mn))
