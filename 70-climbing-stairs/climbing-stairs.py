6451537c3c3a98e6e6ef71d21321250eb380d2dc
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-19 14:13:58 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (68.52%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #bottom-up
-        if n < 4:
-            return n
-        cache = [2,3]
-        for i in range(4, n+1):
-            new = cache[0] + cache[1]
-            cache[0], cache[1] = cache[1], new
-        
-        return cache[-1]
+        iMinus1 = 1
+        iMinus2 = 1
+
+        for i in range(2, n+1): # n+1 because this is number of stairs, that means it not gonna start with 0
+            curr = (iMinus1     # either you can come from 1 step prior
+                    + iMinus2)  # or you can come from 2 steps prior
+
+                                # for next element
+            iMinus2 = iMinus1   # the previous become 2 steps prior
+            iMinus1 = curr      # the current becomes 1 step prior
+    
+        return iMinus1
+
+# At each step, we can reach it either from 1 step prior or 2 step prior. So the ways is the sum of ways to reach those two steps. Remember that n steps doesnt start from 0.
+
+# Time Complexity >> O(n)
+# Space Complexity >> O(1)
