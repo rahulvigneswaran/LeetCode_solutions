dbb2593797de787c94366d54a22a8ce00196e1fc
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2024-10-19 16:07:05 +0530

    Time: 560 ms (90.21%) | Memory: 19.8 MB (99.34%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        
-        SolutionSet = []
-        for i, l0 in enumerate(nums):
-            if l0 > 0:
-                break
-            if i > 0 and (l0 == nums[i-1]):
+        res = []
+        nums.sort()
+
+        for n1, val in enumerate(nums):
+            if n1 > 0 and nums[n1] == nums[n1-1]:
                 continue
             
-            l, r = i+1, len(nums)-1
-            
-            while l < r:
-                ThreeSum = l0 + nums[l] + nums[r]
-                if ThreeSum > 0:
-                    r-=1
-                elif ThreeSum < 0 :
-                    l+=1
+            L = n1 + 1
+            R = len(nums) - 1
+            while L < R:
+                addedSum = nums[n1] + nums[L] + nums[R]
+                if addedSum > 0:
+                    R-=1
+                elif addedSum < 0:
+                    L+=1
                 else:
-                    SolutionSet.append([l0, nums[l], nums[r]])
-                    l+=1
-                    r-=1
-                    while nums[l] == nums[l-1] and l < r:
-                        l+=1
-        return SolutionSet
+                    res.append([nums[n1], nums[L], nums[R]])
+                    L+=1
+                    while nums[L] == nums[L-1] and L < R:
+                        L+=1
+        return res
 
+# Not a TwoSum problem with the first number fixed. This cant have duplicates. Sort the array. Fix the first number. Dont use the first number if it is same as the previous. This means we already used this to find TwoSum. Now for the next two numbers, we can use typical twoSum but we can do better by not using the extra space for hash. We exploit the sorted property and use two pointers. Increase left if <0, decrease right if >0. Here to iterate left if its a duplicate.
 
+# Time complexity >> O(n^2)
+# Space complexity >> O(1)
