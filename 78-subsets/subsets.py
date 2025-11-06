a365bdcafe1a6b3c201b41a776801ed4e84e9e5b
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-06 19:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.66%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        #dfs
+        def helper(ind, subset):
+            if ind >= len(nums):
+                res.append(subset)
+                return
+
+            helper(ind+1, subset)
+            helper(ind+1, subset + [nums[ind]])
+        
+        helper(0, [])
+        return res
