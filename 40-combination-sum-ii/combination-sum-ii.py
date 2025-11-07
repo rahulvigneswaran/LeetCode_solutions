4562fa66f209b257fcbd9f56979e5711c5991e84
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-07 17:37:27 +0530

    Time: 30 ms (26.07%) | Memory: 18 MB (14.32%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,25 +1,28 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        subset = []
         candidates.sort()
 
-        #dfs
-        def helper(ind, total, subset):
+        # dfs
+        def helper(ind, total):
             if total == target:
                 res.append(subset.copy())
-                return 
-            
-            if total > target or ind >= len(candidates):
                 return
+            
+            if ind >= len(candidates) or total > target:
+                return
+            
+            num = candidates[ind]
+            subset.append(num)
+            helper(ind + 1, total + num)
+            subset.pop()
 
-            for i in range(ind, len(candidates)):
-                
-                if i > ind and candidates[i] == candidates[i-1]:
-                    continue
-
-                num = candidates[i]
-
-                helper(i + 1, total + num, subset + [num])
+            while ind + 1 < len(candidates) and candidates[ind] == candidates[ind+1]:
+                ind+=1
+            
+            helper(ind + 1, total)
         
-        helper(0, 0, [])
+        helper(0, 0)
+
         return res
