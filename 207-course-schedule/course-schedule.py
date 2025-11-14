3122123d0aeaec59f012537d848749f8040ae334
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-14 13:29:38 +0530

    Time: 7 ms (46.80%) | Memory: 19.4 MB (40.19%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adjList = {i:[] for i in range(numCourses)}
+        adjList = defaultdict(list)
+
         for i, j in prerequisites:
             adjList[i].append(j)
+        
         visited = set()
 
         def dfs(node):
             if node in visited:
                 return False
-            if adjList[node] == []:
+
+            if not(adjList[node]):
                 return True
-            
+
             visited.add(node)
-            for nei in adjList[node]:
-                if not dfs(nei):
+
+            for child in adjList[node]:
+                if not(dfs(child)):
                     return False
+            
             visited.remove(node)
             adjList[node] = []
+
             return True
 
-        for root in range(numCourses):
-            if not dfs(root):
+        for i in range(numCourses):
+            if not(dfs(i)):
                 return False
+        
         return True
-# Do DFS. Create adjList. If visited, its loop. False. Remove from visited after fully doing DFS and also make it [].
-# Optimized solution >> Time : O(n+e). Space : O(n). n is number of nodes.
