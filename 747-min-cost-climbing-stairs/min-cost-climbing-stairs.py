e1cf7d8184cf6e27a0c47549d4d93ab05c65c5cd
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-16 13:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.04%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -11,20 +11,32 @@ class Solution:
         # return min(dfs(len(cost)-1), dfs(len(cost)-2))
         
         # memoization
-        # top-down
-        dp = [-1]*len(cost)
 
-        def dfs(node):
-            if node <= 1:
-                return cost[node]
+        # # top-down
+        # dp = [-1]*len(cost)
 
-            if dp[node] != -1:
-                return dp[node]
+        # def dfs(node):
+        #     if node <= 1:
+        #         return cost[node]
+
+        #     if dp[node] != -1:
+        #         return dp[node]
             
-            cost_at_node = cost[node] + min(dfs(node-1), dfs(node-2))
-            dp[node] = cost_at_node
+        #     cost_at_node = cost[node] + min(dfs(node-1), dfs(node-2))
+        #     dp[node] = cost_at_node
 
-            return cost_at_node
+        #     return cost_at_node
 
+        # n = len(cost)
+        # return min(dfs(n-1), dfs(n-2))
+
+        # bottom-up
+        dp = cost
         n = len(cost)
-        return min(dfs(n-1), dfs(n-2))
+        if len(cost) == 2:
+            return min(cost)
+
+        for i in range(2, n):
+            dp[i] = dp[i] + min(dp[i-1], dp[i-2])
+
+        return min(dp[n-1], dp[n-2])
