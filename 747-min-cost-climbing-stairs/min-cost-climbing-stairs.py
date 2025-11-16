2ac01abb1d87f335a626e9d3f67e85c45befa336
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-16 14:53:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (29.52%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -30,13 +30,21 @@ class Solution:
         # n = len(cost)
         # return min(dfs(n-1), dfs(n-2))
 
-        # bottom-up
-        dp = cost
+        # # bottom-up
+        # dp = cost
+        # n = len(cost)
+        # if len(cost) == 2:
+        #     return min(cost)
+
+        # for i in range(2, n):
+        #     dp[i] = dp[i] + min(dp[i-1], dp[i-2])
+
+        # return min(dp[n-1], dp[n-2])
+
+        # bottom-up space optimized
+        Minus1, Minus2 = cost[1], cost[0]
         n = len(cost)
-        if len(cost) == 2:
-            return min(cost)
-
         for i in range(2, n):
-            dp[i] = dp[i] + min(dp[i-1], dp[i-2])
-
-        return min(dp[n-1], dp[n-2])
+            Minus1, Minus2 = cost[i] + min(Minus1, Minus2), Minus1
+        
+        return min(Minus1, Minus2)
