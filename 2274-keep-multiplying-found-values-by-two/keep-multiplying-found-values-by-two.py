010999b5ac5f3198a82947e6f39d825a60864390
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-19 10:47:51 +0530

    Time: 3 ms (19.03%) | Memory: 18 MB (35.42%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -7,3 +7,15 @@ class Solution:
                 original *= 2
             else:
                 return original
+
+        # hash set
+        # L = 0
+        # while L < len(nums):
+        #     for i in range(L, len(nums)):
+        #         if nums[i] == original:
+        #             original *= 2
+        #             L = i + 1
+        #         elif nums[i] > original:
+        #             break                    
+
+        # return original
