0506f6a8dc257256f7aebd9583c38cec0892ce82
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-19 12:35:17 +0530

    Time: 995 ms (39.48%) | Memory: 18.2 MB (65.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -34,6 +34,34 @@ class Solution:
 
 
         # bottom-up
+        # total = sum(nums)
+        # n = len(nums)
+
+        # # edge cases
+        # if total % 2 != 0:
+        #     return False
+
+        # target = total // 2
+        
+        # # grid
+        # tab = [[False]*(target + 1) for _ in range(n+1)]
+
+        # # prefill base conditions
+        # for i in range(n+1):
+        #     tab[i][0] = True
+
+        # # iteration
+        # for i in range(1, n+1):
+        #     for j in range(1, target+1): 
+        #         if nums[i-1] <= j:
+        #             tab[i][j] = tab[i-1][j] or tab[i-1][j - nums[i-1]]
+        #         else:
+        #             tab[i][j] = tab[i-1][j]
+                
+        # return tab[n][target]
+
+
+        # bottom-up (space optimized)
         total = sum(nums)
         n = len(nums)
 
@@ -44,21 +72,24 @@ class Solution:
         target = total // 2
         
         # grid
-        tab = [[False]*(target + 1) for _ in range(n+1)]
+        prev = [False]*(target + 1)
+        
 
         # prefill base conditions
-        for i in range(n+1):
-            tab[i][0] = True
+        prev[0] = True
 
         # iteration
         for i in range(1, n+1):
+            curr = [False]*(target + 1)
+            curr[0] = True
             for j in range(1, target+1): 
                 if nums[i-1] <= j:
-                    tab[i][j] = tab[i-1][j] or tab[i-1][j - nums[i-1]]
+                    curr[j] = prev[j] or prev[j - nums[i-1]]
                 else:
-                    tab[i][j] = tab[i-1][j]
+                    curr[j] = prev[j]
+            prev = curr
                 
-        return tab[n][target]
+        return curr[target]
 
         
 
