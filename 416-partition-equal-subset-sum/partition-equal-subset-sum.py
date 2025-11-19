e80a4c364f3bc72894a460330987a8b544473bfe
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-19 12:17:43 +0530

    Time: 1451 ms (30.34%) | Memory: 33.4 MB (35.21%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -13,20 +13,50 @@ class Solution:
         # return dfs(0, sum(nums)//2)
 
         # memoization
-        # top-down
+        
+        # # top-down
 
+        # if sum(nums) % 2 != 0:
+        #     return False
+        
+        # memo = {}
+
+        # def dfs(n, targetSum):
+        #     if n == len(nums):
+        #         return targetSum == 0
+            
+        #     if (n, targetSum) not in memo:
+        #         memo[(n, targetSum)] = dfs(n+1, targetSum - nums[n]) or dfs(n+1, targetSum)
+
+        #     return memo[n, targetSum]
+        
+        # return dfs(0, sum(nums)//2)
+
+
+        # bottom-up
+
+        # edge cases
         if sum(nums) % 2 != 0:
             return False
-        
-        memo = {}
 
-        def dfs(n, targetSum):
-            if n == len(nums):
-                return targetSum == 0
-            
-            if (n, targetSum) not in memo:
-                memo[(n, targetSum)] = dfs(n+1, targetSum - nums[n]) or dfs(n+1, targetSum)
-
-            return memo[n, targetSum]
+        target = sum(nums) // 2
         
-        return dfs(0, sum(nums)//2)
+        # grid
+        tab = [[False]*(target + 1) for _ in range(len(nums)+1)]
+
+        # prefill base conditions
+        for i in range(len(nums)+1):
+            tab[i][0] = True
+
+        # iteration
+        for n in range(1, len(nums)+1):
+            for capacity in range(1, target+1): 
+                if nums[n-1] <= capacity:
+                    tab[n][capacity] = tab[n-1][capacity] or tab[n-1][capacity - nums[n-1]]
+                else:
+                    tab[n][capacity] = tab[n-1][capacity]
+                
+        return tab[len(nums)][target]
+
+
+                     
