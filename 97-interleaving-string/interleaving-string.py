001a517fea3312a92e2a0ac126db0388984ac1de
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-20 21:06:57 +0530

    Time: 42 ms (70.83%) | Memory: 17.8 MB (79.32%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -21,25 +21,47 @@ class Solution:
         # return dfs(0, 0)
 
         # memoization
-        memo = {}
+        # memo = {}
 
-        def dfs(L, R):
-            # base cases
-            if L == len(s1) and R == len(s2):
-                return True
+        # def dfs(L, R):
+        #     # base cases
+        #     if L == len(s1) and R == len(s2):
+        #         return True
             
-            if (L, R) in memo:
-                return memo[(L, R)]
+        #     if (L, R) in memo:
+        #         return memo[(L, R)]
 
-            includeLeft = includeRight = 0
-            # recursion statement
-            if L < len(s1) and s1[L] == s3[L+R]:
-                includeLeft = dfs(L+1, R)
-            if not includeLeft and R < len(s2) and s2[R] == s3[L+R]:
-                includeRight = dfs(L, R+1)
+        #     includeLeft = includeRight = 0
+        #     # recursion statement
+        #     if L < len(s1) and s1[L] == s3[L+R]:
+        #         includeLeft = dfs(L+1, R)
+        #     if not includeLeft and R < len(s2) and s2[R] == s3[L+R]:
+        #         includeRight = dfs(L, R+1)
 
-            memo[(L, R)] = includeLeft or includeRight
+        #     memo[(L, R)] = includeLeft or includeRight
 
-            return includeLeft or includeRight
+        #     return includeLeft or includeRight
         
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        # Tabulation
+        # dims
+        ROWS = len(s1) + 1
+        COLS = len(s2) + 1
+        
+
+        # Table
+        tab = [[False]*COLS for _ in range(ROWS)]
+
+        # base conditions
+        tab[ROWS-1][COLS-1] = True
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1 , -1, -1):
+                if r < len(s1) and s1[r] == s3[r+c]:
+                    tab[r][c] = tab[r+1][c]
+                
+                if not tab[r][c] and c < len(s2) and s2[c] == s3[r+c]:
+                    tab[r][c] = tab[r][c+1]
+                
+        return tab[0][0]
