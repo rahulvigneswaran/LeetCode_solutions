8001fb40d162d6bde358ec3f3c4dc5ba53376dac
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-20 18:26:19 +0530

    Time: 351 ms (6.94%) | Memory: 21.2 MB (47.60%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -14,43 +14,7 @@ class Solution:
         # return dfs(0, 0)
 
         # memoization
-        memo = {}
-        def dfs(n, total):
-            #base case
-            if n == len(nums):
-                return int(total == target)
-            
-            if (n, total) in memo:
-                return memo[(n,total)]
-            
-            # recursion statement
-            res = dfs(n+1, total+nums[n]) + dfs(n+1, total-nums[n])
-
-            memo[(n,total)] = res
-            return res
-        
-        return dfs(0, 0)
-        
-        # Tabulation
-        # dims
-        ROWS = len(nums) + 1
-        COLS = target + 1
-        
-        # table
-        tab = [defaultdict(int) for _ in range(ROWS)]
-
-        # base condition
-        for r in range(ROWS):
-            tab[target] = 1
-
-        # ROWS: last -> 0
-        # COLS: last -> 0
-        for r in range(ROWS-2, -1, -1):
-            for c in range(COLS-1, -1, 1):
-                tab[r][c] = tab[r+1][c + nums[r]] + tab[r+1][c - nums[r]]
-        
-        return tab[0][0]
-
+        # memo = {}
         # def dfs(n, total):
         #     #base case
         #     if n == len(nums):
@@ -66,3 +30,23 @@ class Solution:
         #     return res
         
         # return dfs(0, 0)
+        
+        # Tabulation
+        # dims
+        ROWS = len(nums) + 1
+        COLS = sum(nums) + 1 #target + 1
+        
+        # table
+        tab = [defaultdict(int) for _ in range(ROWS)]
+
+        # base condition
+        # for r in range(ROWS):
+        tab[len(nums)][target] = 1
+
+        # ROWS: last -> 0
+        # COLS: last -> 0
+        for r in range(ROWS-2, -1, -1):
+            for c in range(COLS, -COLS, -1):
+                tab[r][c] = tab[r+1][c + nums[r]] + tab[r+1][c - nums[r]]
+        
+        return tab[0][0]
