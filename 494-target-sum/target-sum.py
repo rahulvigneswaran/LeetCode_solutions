671fc72d8ef836844e52114a0db017b9349e2c43
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-20 18:08:49 +0530

    Time: 192 ms (33.48%) | Memory: 38.4 MB (29.93%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -31,3 +31,38 @@ class Solution:
         
         return dfs(0, 0)
         
+        # Tabulation
+        # dims
+        ROWS = len(nums) + 1
+        COLS = target + 1
+        
+        # table
+        tab = [defaultdict(int) for _ in range(ROWS)]
+
+        # base condition
+        for r in range(ROWS):
+            tab[target] = 1
+
+        # ROWS: last -> 0
+        # COLS: last -> 0
+        for r in range(ROWS-2, -1, -1):
+            for c in range(COLS-1, -1, 1):
+                tab[r][c] = tab[r+1][c + nums[r]] + tab[r+1][c - nums[r]]
+        
+        return tab[0][0]
+
+        # def dfs(n, total):
+        #     #base case
+        #     if n == len(nums):
+        #         return int(total == target)
+            
+        #     if (n, total) in memo:
+        #         return memo[(n,total)]
+            
+        #     # recursion statement
+        #     res = dfs(n+1, total+nums[n]) + dfs(n+1, total-nums[n])
+
+        #     memo[(n,total)] = res
+        #     return res
+        
+        # return dfs(0, 0)
