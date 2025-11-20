f361d241b52c45204eb7bcd7965e23f07d26020c
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-20 21:23:57 +0530

    Time: 40 ms (80.31%) | Memory: 17.9 MB (67.16%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -45,23 +45,49 @@ class Solution:
         # return dfs(0, 0)
 
         # Tabulation
-        # dims
+        # # dims
+        # ROWS = len(s1) + 1
+        # COLS = len(s2) + 1
+        
+
+        # # Table
+        # tab = [[False]*COLS for _ in range(ROWS)]
+
+        # # base conditions
+        # tab[ROWS-1][COLS-1] = True
+
+        # for r in range(ROWS - 1, -1, -1):
+        #     for c in range(COLS - 1 , -1, -1):
+        #         if r < len(s1) and s1[r] == s3[r+c]:
+        #             tab[r][c] = tab[r+1][c]
+                
+        #         if not tab[r][c] and c < len(s2) and s2[c] == s3[r+c]:
+        #             tab[r][c] = tab[r][c+1]
+                
+        # return tab[0][0]
+
+        # Tabulation (Space optimized)
+                # dims
         ROWS = len(s1) + 1
         COLS = len(s2) + 1
         
 
         # Table
-        tab = [[False]*COLS for _ in range(ROWS)]
+        prev = [False]*COLS
 
         # base conditions
-        tab[ROWS-1][COLS-1] = True
+        prev[COLS-1] = True
+        for c in range(COLS - 2, -1, -1):
+            prev[c] = prev[c+1] and s2[c] == s3[len(s1) + c]
 
-        for r in range(ROWS - 1, -1, -1):
+        for r in range(ROWS - 2, -1, -1):
+            curr = [False]*COLS
             for c in range(COLS - 1 , -1, -1):
                 if r < len(s1) and s1[r] == s3[r+c]:
-                    tab[r][c] = tab[r+1][c]
+                    curr[c] = prev[c]
                 
-                if not tab[r][c] and c < len(s2) and s2[c] == s3[r+c]:
-                    tab[r][c] = tab[r][c+1]
+                if not curr[c] and c < len(s2) and s2[c] == s3[r+c]:
+                    curr[c] = curr[c+1]
                 
-        return tab[0][0]
+            prev = curr
+        return prev[0]
