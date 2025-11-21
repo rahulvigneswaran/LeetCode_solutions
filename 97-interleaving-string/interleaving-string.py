8bce11e667e69a2d3332aed28d03b5765c8410e3
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-11-21 11:29:32 +0530

    Time: 44 ms (60.57%) | Memory: 18.1 MB (45.85%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -45,18 +45,42 @@ class Solution:
         # return dfs(0, 0)
 
         # Tabulation
-        # # dims
+        # dims
+        ROWS = len(s1) + 1
+        COLS = len(s2) + 1
+        
+
+        # Table
+        tab = [[False]*COLS for _ in range(ROWS)]
+
+        # base conditions
+        tab[ROWS-1][COLS-1] = True
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1 , -1, -1):
+                if r < len(s1) and s1[r] == s3[r+c]:
+                    tab[r][c] = tab[r+1][c]
+                
+                if not tab[r][c] and c < len(s2) and s2[c] == s3[r+c]:
+                    tab[r][c] = tab[r][c+1]
+                
+        return tab[0][0]
+
+
+
+        # dims
         # ROWS = len(s1) + 1
         # COLS = len(s2) + 1
         
 
         # # Table
-        # tab = [[False]*COLS for _ in range(ROWS)]
+        # prev = [False]*COLS
 
         # # base conditions
-        # tab[ROWS-1][COLS-1] = True
+        # prev[COLS-1] = True
 
-        # for r in range(ROWS - 1, -1, -1):
+        # for r in range(ROWS - 2, -1, -1):
+        #     curr[False]*COLS
         #     for c in range(COLS - 1 , -1, -1):
         #         if r < len(s1) and s1[r] == s3[r+c]:
         #             tab[r][c] = tab[r+1][c]
@@ -67,27 +91,27 @@ class Solution:
         # return tab[0][0]
 
         # Tabulation (Space optimized)
-                # dims
-        ROWS = len(s1) + 1
-        COLS = len(s2) + 1
+        #         # dims
+        # ROWS = len(s1) + 1
+        # COLS = len(s2) + 1
         
 
-        # Table
-        prev = [False]*COLS
+        # # Table
+        # prev = [False]*COLS
 
-        # base conditions
-        prev[COLS-1] = True
-        for c in range(COLS - 2, -1, -1):
-            prev[c] = prev[c+1] and s2[c] == s3[len(s1) + c]
+        # # base conditions
+        # prev[COLS-1] = True
+        # for c in range(COLS - 2, -1, -1):
+        #     prev[c] = prev[c+1] and s2[c] == s3[len(s1) + c]
 
-        for r in range(ROWS - 2, -1, -1):
-            curr = [False]*COLS
-            for c in range(COLS - 1 , -1, -1):
-                if r < len(s1) and s1[r] == s3[r+c]:
-                    curr[c] = prev[c]
+        # for r in range(ROWS - 2, -1, -1):
+        #     curr = [False]*COLS
+        #     for c in range(COLS - 1 , -1, -1):
+        #         if r < len(s1) and s1[r] == s3[r+c]:
+        #             curr[c] = prev[c]
                 
-                if not curr[c] and c < len(s2) and s2[c] == s3[r+c]:
-                    curr[c] = curr[c+1]
+        #         if not curr[c] and c < len(s2) and s2[c] == s3[r+c]:
+        #             curr[c] = curr[c+1]
                 
-            prev = curr
-        return prev[0]
+        #     prev = curr
+        # return prev[0]
