b47d251ac06a5589ec3d834211b3e442ec53f6c9
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2025-12-05 11:31:49 +0530

    Time: 7 ms (42.80%) | Memory: 18.9 MB (38.54%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,42 +1,55 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # recursive
-        # def dfs(n, canBuy):
-        #     if n == len(prices):
+        # # naive
+        # # dfs
+        # N = len(prices)
+        # def dfs(ind, is_buy):
+        #     if ind >= N:
         #         return 0
+            
+        #     # skip
+        #     max_p = dfs(ind+1, is_buy)
 
-        #     cooldown = dfs(n+1, canBuy)
-
-        #     if canBuy:
-        #         profit = dfs(n+1, not canBuy) - prices[n]
+        #     # buy
+        #     if is_buy:
+        #         p = dfs(ind+1, False) - prices[ind]
+        #         max_p = max(max_p, p)
         #     else:
-        #         profit = dfs(n+2, not canBuy) + prices[n]
+        #         p = dfs(ind+2, True) + prices[ind]
+        #         max_p = max(max_p, p)
             
-        #     return max(profit, cooldown)
+        #     return max_p
 
-        # return dfs(0, True)
-        # memoization
-        memo = {}
-
-        def dfs(n, canBuy):
-            if n >= len(prices):
-                return 0
-
-            if (n, canBuy) in memo:
-                return memo[(n, canBuy)]
-
-            cooldown = dfs(n+1, canBuy)
-
-            if canBuy:
-                profit = dfs(n+1, not canBuy) - prices[n]
-            else:
-                profit = dfs(n+2, not canBuy) + prices[n]
-            
-            memo[(n, canBuy)] = max(profit, cooldown)
-            return memo[(n, canBuy)]
-
-        return dfs(0, True)
-
-        # tabulation
         
+        # return dfs(0, True)
+
+        # memo
+        memo = {}
+        # dfs
+        N = len(prices)
+        def dfs(ind, is_buy):
+            if ind >= N:
+                return 0
+            
+            if (ind, is_buy) in memo:
+                return memo[(ind, is_buy)]
+
+            # skip
+            max_p = dfs(ind+1, is_buy)
+
+            # buy
+            if is_buy:
+                p = dfs(ind+1, False) - prices[ind]
+                max_p = max(max_p, p)
+            else:
+                p = dfs(ind+2, True) + prices[ind]
+                max_p = max(max_p, p)
+            
+            memo[(ind, is_buy)] = max_p
+            
+            return max_p
+
+        
+        return dfs(0, True)
+        
+
