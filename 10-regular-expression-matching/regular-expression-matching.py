a2abf6b4eac4495ce76b6894c7c8c53122d27bf2
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2026-02-14 18:50:36 +0530

    Time: 6 ms (69.13%) | Memory: 20.5 MB (19.41%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,7 +1,32 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        # naive
+        # def dfs(i, j):
+        #     if i >= len(s) and j >= len(p):
+        #         return True
+            
+        #     if j >= len(p):
+        #         return False
+            
+        #     match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+        #     if (j + 1 < len(p) and (p[j+1] == "*")):
+        #         return (dfs(i, j+2) or # not use *
+        #                 (match and dfs(i+1, j))) # use *
+
+        #     if match:
+        #         return dfs(i+1, j+1)
+            
+        #     return False
         
+        # return dfs(0, 0)
+
+        # memo
+        memo = {}
         def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
             if i >= len(s) and j >= len(p):
                 return True
             
@@ -11,11 +36,13 @@ class Solution:
             match = i < len(s) and (s[i] == p[j] or p[j] == ".")
 
             if (j + 1 < len(p) and (p[j+1] == "*")):
-                return (dfs(i, j+2) or # not use *
-                        (match and dfs(i+1, j))) # use *
+                memo[(i, j)] = (dfs(i, j+2) or # not use *
+                                (match and dfs(i+1, j))) # use *
+                return memo[(i, j)]
 
             if match:
-                return dfs(i+1, j+1)
+                memo[(i, j)] =  dfs(i+1, j+1)
+                return memo[(i, j)]
             
             return False
         
