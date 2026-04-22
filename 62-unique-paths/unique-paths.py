31c4b42a2649ae6b0d6485b0bd6c2be1f336fe34
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2026-04-22 15:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (47.54%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,61 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        rows = [0]*n
-        rows[-1] = 1
-        grid = [rows]*m
-        grid[-1] = [1]*n
+        # naive recursion
+        # def helper(R, C):
+        #     if R == m-1 and C == n-1:
+        #         return 1
+            
+        #     a = b = 0
 
-        for r in range(m-2, -1, -1):
-            for c in range(n-2, -1, -1):
-                grid[r][c] = grid[r+1][c] + grid[r][c+1]
+        #     if R + 1 < m:
+        #         a = helper(R+1, C)
+        #     if C + 1 < n:
+        #         b = helper(R, C+1)
+            
+        #     return a + b
+        # return helper(0, 0)
+
+        # memo
+        # memo = {}
+        # def helper(R, C):
+        #     if R == m-1 and C == n-1:
+        #         return 1
+            
+        #     if (R, C) in memo:
+        #         return memo[(R, C)]
+        #     a = b = 0
+
+        #     if R + 1 < m:
+        #         a = helper(R+1, C)
+        #     if C + 1 < n:
+        #         b = helper(R, C+1)
+            
+        #     memo[(R, C)] = a + b
+        #     return memo[(R, C)]
+        # return helper(0, 0)
+    
+        # tab
+        # tab = [[0]*(n+1) for _ in range(m+1)]
+
+        # for R in range(m-1, -1, -1):
+        #     for C in range(n-1, -1, -1):
+        #         if R == m-1 and C == n-1:
+        #             tab[R][C] = 1
+        #         else:
+        #             tab[R][C] = tab[R+1][C] + tab[R][C+1]
         
-        return grid[0][0]
-                
+        # return tab[0][0]
+
+        # tab (space optimized)
+        prevtab = [1]*(n+1)
+
+        for _ in range(m-2, -1, -1):
+            currtab = [0]*(n+1)
+            for C in range(n-1, -1, -1):
+                currtab[C] = prevtab[C] + currtab[C+1]
+            prevtab = currtab
+        
+        return prevtab[0]
+        
+
         
