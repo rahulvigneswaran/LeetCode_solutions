d425bfbc4f881744f8e8d67cde579b7706c9ad4c
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2026-04-30 11:38:46 +0530

    Time: 535 ms (35.92%) | Memory: 52.7 MB (41.93%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -18,23 +18,43 @@ class Solution:
         # return helper(0, 0)
         
         # memo
-        memo = {}
-        def helper(ind, total):
-            if amount == total:
-                return 1
-            if total > amount or ind == len(coins):
-                return 0
+        # memo = {}
+        # def helper(ind, total):
+        #     if amount == total:
+        #         return 1
+        #     if total > amount or ind == len(coins):
+        #         return 0
 
-            if (ind, total) in memo:
-                return memo[(ind, total)]
+        #     if (ind, total) in memo:
+        #         return memo[(ind, total)]
 
-            # include
-            a = helper(ind, total + coins[ind])
+        #     # include
+        #     a = helper(ind, total + coins[ind])
 
-            # skip
-            b = helper(ind+1, total)
+        #     # skip
+        #     b = helper(ind+1, total)
             
-            memo[(ind, total)] = a + b
-            return a + b
+        #     memo[(ind, total)] = a + b
+        #     return a + b
         
-        return helper(0, 0)
+        # return helper(0, 0)
+
+        # tab 
+        tab = [[0]*(amount + 1) for _ in range(len(coins)+1)]
+
+        # base case
+        for ind in range(len(coins)+1):
+            tab[ind][amount] = 1
+
+        for ind in range(len(coins)-1, -1, -1):
+            for total in range(amount, -1, -1):
+
+                include = 0
+                if total + coins[ind] <= amount:
+                    include = tab[ind][total + coins[ind]]
+
+                skip = 0
+                skip = tab[ind+1][total]
+
+                tab[ind][total] = include + skip        
+        return tab[0][0]
